f03545677630a5f61247a38ca4c7951221527304
rogofan <sacokov@gmail.com>
2023-08-13 22:26:41 +0200

    NameSpace version

---
+++ src/models/Project.ts
@@ -0,0 +1,17 @@
+namespace App {
+  // project type
+  export enum ProjectStatus {
+    Active,
+    finished,
+  }
+
+  export class Project {
+    constructor(
+      public id: string,
+      public title: string,
+      public description: string,
+      public people: number,
+      public status: ProjectStatus
+    ) {}
+  }
+}
